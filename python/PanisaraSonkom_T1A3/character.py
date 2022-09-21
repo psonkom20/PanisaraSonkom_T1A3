875db2b34b1e14c4b5f0ede7c9eafec4765e44ef
Panisara Sonkom <[psonkom342@gmail.com]>
2022-09-21 18:14:55 +1000

    Restructure character dict

--- python/PanisaraSonkom_T1A3/character.py
+++ python/PanisaraSonkom_T1A3/character.py
@@ -1,20 +1,77 @@
-# Create characters dictionary
-characters = {
-   'Cersei Lannister' : "The villain from beginning to end. This character manipulates, betrays, and fight her way to the top. This character famously said, “When you play the game of thrones, you win or you die”",
-   'Jon Snow' : "He was known as the bastard son. But his honorable character and strive to always do what's right made him the King of the North. This character famously said, “You'll be ruling over a graveyard if we don't defeat the Night King”",
-   'Daenerys Targaryen': "Her end, daresay, left us all feeling anger. She embrace her true self against all odds. This character never stop fighting or give up for the thone. This character famously said, “I will take back what is mine with fire and blood”",
-   'Sansa Stark': "We slowly learned to love this character. She's someone who learned to weaponize the ways of the court and master the art of deceit. This character famously said, “Your words will disappear. Your house will disappear. Your name will disappear. All memory of you will disappear”",
-   'Arya Stark' : "This character is deadly and will avenge those who wronged her and her family. This character famously said, “When people ask you what happened here, tell them the North remembers. Tell them winter came for House Frey”",
-   'Bran Stark' : "This character can see the future, the past, and distant events. This character famously said, “An endless night. He wants to erase this world, and I am its memory”",
-   'Joffrey Baratheon' : "Seeing this character makes your blood boil. This character famously said, “Everyone is mine to torment”",
-   'Ramsay Bolton' : "The legitimzed bastard whose end needs no justification. This character famously said, “Let's play a game”",
-   'Night King' : "His chilling presence commands all armies to unite. This character is a man of action",
-   'Tyrion Lannister': "A clever, witty, and resilient character. This character faces revulsion from most his family his entire life. This character famously said, “Never forget what your are, the rest of the world will not. Wear it like armor and it can never be used to hurt you”",
-   'Jaime Lannister' : "For many, this character symbolizes redemption or… that people can change after hacing a limb chopped off. This character famously said, “By what right does the wolf judge the lion”",
-   'Brienne of Tarth' : "A one true knight. This character holds true to her oath. This character famously said, “I will shield your back and keep your counsel and give my life for yours if need be”",
-   'Olenna Tyrell' : "She plays the game with style and grace. Many describes her as a cunning woman with a wicked wit and sharp tongue. This character famously said, “Tell Cersei. I want her to know it was me”",
-   'Lyanna Mormont' : "A fierce little lady who bravely fought and died defending the North. This character famously said, “We know no king but the King in the North whose name is Stark”",
-   'Little Finger' : "He's cunning and calculating. This character manipulates his way to the top. This character famously said, “So many men, they risk so little. They spend their lives avoiding danger. And then they die. I'd risk everything to get what I want”",
+# Create characters dictionary; include character's quote and hint for each character
+char1 = {
+   'Name' : 'Cersei Lannister', 
+   'Quote':  "When you play the game of thrones, you win or you die",
+   'Hint' : "The villain from beginning to end. This character manipulates, betrays, and fight her way to the top.",
+}
+char2 = {
+   'Name' : 'Jon Snow',
+   'Quote': "You'll be ruling over a graveyard if we don't defeat the Night King", 
+   'Hint' : "He was known as the bastard son. But his honorable character and strive to always do what's right made him the King of the North.",
+}
+char3 = {
+   'Name' : 'Daenerys Targaryen',
+   'Quote': "I will take back what is mine with fire and blood", 
+   'Hint' : "Her end, daresay, left us all feeling anger. She embrace her true self against all odds. This character never stop fighting or give up for the thone.",
+}
+char4 = {
+   'Name' : 'Sansa Stark',
+   'Quote': "Your words will disappear. Your house will disappear. Your name will disappear. All memory of you will disappear.", 
+   'Hint' : "We slowly learned to love this character. She's someone who learned to weaponize the ways of the court and master the art of deceit.",
+}
+char5 = {
+   'Name' : 'Arya Stark',
+   'Quote': "When people ask you what happened here, tell them the North remembers. Tell them winter came for House Frey.",
+   'Hint' : "This character is deadly and will avenge those who wronged her and her family.",
+}
+char6 = {
+   'Name' : 'Bran Stark',
+   'Quote' : "This character can see the future, the past, and distant events.",
+   'Hint': "An endless night. He wants to erase this world, and I am its memory.",   
+}
+char7 = {
+   'Name' : 'Joffrey Baratheon',
+   'Quote' : "Everyone is mine to torment",
+   'Hint': "Seeing this character makes your blood boil.",   
+}
+char8 = {
+   'Name' : 'Ramsay Bolton',
+   'Quote': "Let's play a game", 
+   'Hint' : "The legitimzed bastard whose end needs no justification. This character famously said",
+}
+char9 = {
+   'Name' : 'Night King',
+   'Quote': "He never speaks, but his chilling presence commands all armies to unite.", 
+   'Hint' : "He's the first of his kind",
+}
+char10 = {
+   'Name' : 'Tyrion Lannister',
+   'Quote': "Never forget what your are, the rest of the world will not. Wear it like armor and it can never be used to hurt you", 
+   'Hint' : "A clever, witty, and resilient character. This character faces revulsion from most his family his entire life.",
+}
+char11 = {
+   'Name' : 'Jaime Lannister',
+   'Quote': "By what right does the wolf judge the lion", 
+   'Hint' : "For many, this character symbolizes redemption or… that people can change after hacing a limb chopped off.",
+}
+char12 = {
+   'Name' : 'Brienne of Tarth',
+   'Quote':  "I will shield your back and keep your counsel and give my life for yours if need be",
+   'Hint' : "A one true knight. This character holds true to her oath.",
+}
+char13 = {
+   'Name' : 'Olenna Tyrell',
+   'Quote': "Tell Cersei. I want her to know it was me.",
+   'Hint' : "She plays the game with style and grace. Many describes her as a cunning woman with a wicked wit and sharp tongue.",
+}
+char14 = {
+   'Name' : 'Lyanna Mormont',
+   'Quote': "We know no king but the King in the North whose name is Stark.",
+   'Hint' : "A fierce little lady who bravely fought and died defending the North.",
+}
+char15 = {
+   'Name' : 'Little Finger',
+   'Quote': "So many men, they risk so little. They spend their lives avoiding danger. And then they die. I'd risk everything to get what I want.", 
+   'Hint' : "He's cunning and calculating. This character manipulates his way to the top.",
 }
 
-print(characters.values())
--- python/PanisaraSonkom_T1A3/main.py
+++ python/PanisaraSonkom_T1A3/main.py
@@ -1,3 +1,5 @@
+#import random package to enable randomise character function
+#import cowsay to add graphic
 import cowsay
 
 cowsay.dragon('Are you ready to fight for the throne!!')
