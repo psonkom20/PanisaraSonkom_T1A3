94babd07391621642eb02f1b7864c8d3382b8f8f
Panisara Sonkom <[psonkom342@gmail.com]>
2022-09-22 18:47:30 +1000

    Added feature 1 which enable randomisation of question or; user can select amount of q

--- python/PanisaraSonkom_T1A3/finalmain.py
+++ python/PanisaraSonkom_T1A3/finalmain.py
@@ -1,4 +1,6 @@
-#Main game structure
+#import random package to allow question order to be randomise
+import random
+#Main game structure and functionality
 def new_game()
     questions = prep_questions(
         QUESTION, number_questions = NUMBER_QUESTION_PER_QUIZ
@@ -9,3 +11,9 @@ def new_game()
         print(f"question {num}:")
         num_correct += ask_question(question, alternatives)
     print(f"You got {num_correct} out of {num} questions correct!!")
+
+#Randomise questions order to add difficulty for player who is playing multiple time
+#Allow user to select however many questions they want; IMPORTANT: max 15 questions
+def prep_questions(questions, number_questions)
+    number_questions = min(number_questions, len(questions))
+    return random.sample(list(questions.items()), k=number_questions)
