de5e5c1fc4b35b1a61786b5300e5bed85d17d16e
Panisara Sonkom <[psonkom342@gmail.com]>
2022-09-22 19:22:45 +1000

    allow user input; ensure correct answer is retrieve from dict; check user answer

--- python/PanisaraSonkom_T1A3/finalmain.py
+++ python/PanisaraSonkom_T1A3/finalmain.py
@@ -1,9 +1,10 @@
 #import random package to allow question order to be randomise
 import random
+from finalcharacter import char_question
 #Main game structure and functionality
 def new_game()
-    questions = prep_questions(
-        QUESTION, number_questions = NUMBER_QUESTION_PER_QUIZ
+    questions = prep_char_questions(
+        char_question, number_questions = NUMBER_QUESTION_PER_QUIZ
     )
 
     num_correct = 0
@@ -14,6 +15,24 @@ def new_game()
 
 #Randomise questions order to add difficulty for player who is playing multiple time
 #Allow user to select however many questions they want; IMPORTANT: max 15 questions
-def prep_questions(questions, number_questions)
+def prep_char_questions(questions, number_questions)
     number_questions = min(number_questions, len(questions))
     return random.sample(list(questions.items()), k=number_questions)
+
+#Ensure that the specific answer of the randomise question list is selected
+#Display all choices of the questions
+#Allow user to input their answer
+# check user's answer
+#add num_correct count
+def ask_char_questions(question, alternatives):
+    correct_ans = alternative[0]
+    ordered_alternatives = random.sample(alternatives, k=len(alternatives))
+
+    answer = get_answer(question, ordered_alternatives)
+    if answer == correct_ans:
+        print("Correct!! You have a good chance on the Throne")
+        return 1
+    else:
+        print(Tough luck! the answer was {correct_ans})
+        return 0
+
