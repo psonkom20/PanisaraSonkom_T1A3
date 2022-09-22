fad47d20d31c6117ae70699b061d94e3ccedac15
Panisara Sonkom <[psonkom342@gmail.com]>
2022-09-22 14:58:16 +1000

    Restructure game

--- python/PanisaraSonkom_T1A3/main.py
+++ python/PanisaraSonkom_T1A3/main.py
@@ -1,47 +1,77 @@
 #import random package to enable randomise character function
 #import cowsay to add graphic
-#import character list from character file
-import random
+# #import character list from character file
+# import random
 import cowsay
-from character import charList
-
-#Main menu
-#instruction of game
-#implement 'ready' so that user can enter the game
-user_command = ''
-cowsay.dragon("Are you ready to fight for the throne?!! This game is simple, but a word of warning, do not underestimate it. Player who dare plays your task is simple. You will be given a quote and all you have to do is enter the name of the character who said the quote. IMPORTANT: You must write the character full name to pass. For each round you will have 20 seconds to guess. You will also have 2 chances to guess for each round. If you are stuck type 'hint' to receive a clue. But use it carefully as there are only 3 hints available for the entire game. To win you must pass all 10 rounds. Goodluck!")
-user_command = print(input("If you are ready enter 'ready': "))
-#code to randomly generate a character from any dictionary
-chosen_character = charList[random.randint(0,14)]
+from newcharacter import char_questions, answer_options
 
 
-
-#Main game set up
-#Add 'quit' to let user quit at anytime
-#Implement 'hint' function to allow user access to clue; limit: 3 hints
-round_count = 0
-guess_count = 0
-
-while round_count < 10:
-    if guess_count == 0:
-        cowsay.dragon(chosen_character['Quote'])
-        answer = print(input("Which character said this?: "))
-        guess_count += 1
-        if answer != chosen_character['Name'] and guess_count < 2:
-            answer = print(input("Try again! Which character said this? You have one chance left: "))
-            guess_count += 1
-            if answer == chosen_character['Name']:
-                print("You are correct")
-                guess_count += 1
-            else:
-                cowsay.dragon('Game over: You loss your chance to fight for the Throne')
-        elif answer == chosen_character['Name']:
-            print("You are correct!")
-            round_count += 1
-        
-        else:
-            cowsay.dragon('Game over: You loss your chance to fight for the Throne')
-            break
     
-   
+#Main game set up
 
+#Display all questions and answer options to user one by one
+#Allow user to input answer and ensure that they can input lowercase or uppercase
+#show user result after completeing the task
+def new_game():
+    user_guesses = []
+    correct_guesses = 0
+    question_number = 1
+
+    for key in char_questions:
+        print("--------------------")
+        cowsay.dragon(key)
+        for i in answer_options[question_number-1]:
+            print(i)
+        user_guess = input("Choose carefully! Enter(A, B, C, or D): ")
+        user_guess = user_guess.upper()
+        user_guesses.append(user_guess)
+
+        correct_guesses += check_answer(char_questions.get(key), user_guess)
+        question_number += 1
+    display_score(correct_guesses,user_guesses)
+#display_score(correct_guesses, user_guess)
+#check user input with dictionary value
+def check_answer(answer,user_guess):
+
+    if answer == user_guess:
+        print("Correct!! The Throne is nearly yours")
+        return 1
+    else:
+        print("Wrong! Careful, you are getting reckless.")
+        return 0
+#let user see their result
+def display_score(correct_guesses, user_guesses):
+    print("--------------------")
+    cowsay.dragon("YOUR RESULTS")
+    print("--------------------")
+
+    print("Answer key", end="")
+    for i in char_questions:
+        print(char_questions.get(i), end = " ")
+    print()
+
+    print("Your Guesses: ", end = "")
+    for i in user_guesses:
+        print(i, end= " ")
+    print()
+
+    total_score = int((correct_guesses/len(char_questions))*100)
+    cowsay.dragon("You got:" + str(total_score) + "%!! Congratulations!")
+#Allow user to play again
+def play_again():
+    user_command = input("Do you want to try and fight for the Throne again? (yes or no): ")
+    user_command = user_command.upper()
+
+    if user_command == "yes":
+        return True
+    else:
+        return False
+
+
+
+new_game()
+
+while play_again():
+    new_game()
+
+print("See you again!!")
---
+++ python/PanisaraSonkom_T1A3/newcharacter.py
@@ -0,0 +1,38 @@
+
+
+char_questions = {
+    "Who said, 'When you play the game of thrones, you win or you die'?:": "B",
+    "Who said, 'You'll be ruling over a graveyard if we don't defeat the Night King'?:": "A",
+    "Who said, 'I will take back what is mine with fire and blood?':": "A",
+    "Who said, 'Your words will disappear. Your house will disappear. Your name will disappear. All memory of you will disappear'?:": "D",
+    "Who said, 'When people ask you what happened here, tell them the North remembers. Tell them winter came for House Frey'?:": "C",
+    "Who said, 'An endless night. He wants to erase this world, and I am its memory'?:": "B",
+    "Who said, 'Everyone is mine to torment'?:": "D",
+    "Who said, 'Let's play a game. Run...'?:": "C",
+    "He doesn't speak but his chilling presence commands all armies to unite. Who is he?:": "A",
+    "Who said, 'Never forget what you are, the rest of the world will not. Wear it like armor and it can never be used to hurt you'?:": "B",
+    "Who said, 'By what right does the wolf judge the lion'?:": "B",
+    "Who said, 'I will shield your back and keep your counsel and give my life for yours if need be'?:": "A",
+    "Who said, 'Tell Cersei. I want her to know it was me'?:": "D",
+    "Who said, 'We know no king but the King in the North whose name is Stark'?:": "C",
+    "Who said, 'So many men, they risk so little. They spend their lives avoiding danger. And then they die. I'd risk everything to get what I want'?:": "D",
+}
+
+answer_options = [
+    ["A. Olenna Tyrell", "B. Cersei Lannister", "C. Tyrion Lannister", "D. Catelyn Stark"],
+    ["A. Jon Snow", "B. Tyrion Lannister", "C. Theon Greyjoy", "D. Bran Stark"],
+    ["A. Daenerys Targaryen", "B. Rhaegar Targaryen", "C. Rhaenyra Targaryen", "D. Daemon Targaryen"],
+    ["A. Jon Snow", "B. Margaery Tyrell", "C. Catelyn Stark", "D. Sansa Stark"],
+    ["A. Rob Stark", "B. Lyanna Mormont", "C. Arya Stark", "D. Sansa Stark"],
+    ["A. Lord varys", "B. Bran Stark", "C. Melisandre", "D. High Sparrow"],
+    ["A. Ramsay Bolton", "B. Walder Frey", "C. Craster", "D. Joffrey Baratheon"],
+    ["A. Cersei Lannister", "B. Little Finger", "C. Ramsay Bolton", "D. Joffrey Baratheon"],
+    ["A. Night King", "B. Viserion ", "C. Ned Stark", "D. Hodor"],
+    ["A. Jaime Lannister", "B. Tyrion Lannister", "C. Little Finger", "D. Brienne of Tarth"],
+    ["A. Cersei Lannister", "B. Jaime Lannister", "C. Tywin Lannister", "D. Tyrion Lannister"],
+    ["A. Brienne of Tarth", "B. Jorah Mormont", "C. Sir Davos", "D. Daario Naharis"],
+    ["A. Sansa Stark", "B. Tyrion Lannister", "C. Arya Stark", "D. Olenna Tyrell"],
+    ["A. Theon Greyjoy", "B. Meera Reed", "C. Lyanna Mormont", "D. Benjen Stark"],
+    ["A. Tyrion Lannister", "B. Olenna Tyrell", "C. Cersei Lannister", "D. Little Finger"],
+]
+
