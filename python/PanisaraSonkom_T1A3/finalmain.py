23b145a78538217b98432697338c9f2685a60f9a
Panisara Sonkom <[psonkom342@gmail.com]>
2022-09-24 02:49:54 +1000

    apply pep8 style guide

--- python/PanisaraSonkom_T1A3/finalmain.py
+++ python/PanisaraSonkom_T1A3/finalmain.py
@@ -1,48 +1,59 @@
-#import random package to allow question order to be randomise
 import random
+from string import ascii_uppercase
 import cowsay
 from finalcharacter import char_question
-from string import ascii_uppercase
+
+
+
 
 #Welcome page
 #Add instruction of game
 #Ask user how many question they want
-cowsay.dragon("Welcome! Are you ready to fight for the Throne?\n The game is simple. You'll be given a quote and\n ask which character said it. You have four\n character options but one chance to guess so\n answer carefully!! Goodluck!")
+cowsay.dragon("Welcome! Are you ready to fight for the Throne?\n The game is simple." \
+"You'll be given a quote and\n ask which character said it. You have four\n character"\
+"options but one chance to guess so\n answer carefully!! Goodluck!")
 def get_int():
+    '''let user input how many question they want'''
     return int(input("How many questions would you like? There is a maximum of 15 question: "))
-while True:     
+while True:
     try:
         user_number_q_per_game = get_int()
         if user_number_q_per_game <= 15:
-            print("------------------------------------------------------------------------------------------------------------------------")
+            print("------------------------------------------------"\
+            "------------------------------------------------------------------------")
             print(f"This quiz will have {user_number_q_per_game} questions")
             break
         if user_number_q_per_game < 1:
             print("input cannot be 0")
         else:
             print("There is a maximum of 15 questions")
-        #print("------------------------------------------------------------------------------------------------------------------------")
-        #print(f"This quiz will have {user_number_q_per_game} questions")
     except (ValueError,ZeroDivisionError,UnboundLocalError) as error:
         print('Provide an integer value')
 
 #Main game structure and functionality
 def new_game():
+    '''main game function'''
     questions = prep_char_questions(
         char_question, user_number_q_per_game)
     num_correct = 0
     for num, (question, alternatives) in enumerate(questions, start=1):
-        print("------------------------------------------------------------------------------------------------------------------------")
+        print("------------------------------------------------"\
+            "------------------------------------------------------------------------")
         print(f"Question {num}:")
         num_correct += ask_char_questions(question, alternatives)
     total_score_perc = int((num_correct/num)*100)
-    print("------------------------------------------------------------------------------------------------------------------------")
-    cowsay.dragon(f"You completed the quiz!! You got {num_correct} out of {num}\n questions. A " +str(total_score_perc) + " %!! You can only sit on the\n throne if you got all the question. So if\n you did, Congrats!!! The Throne is yours!!!\n If not try againg!!")
-    print("------------------------------------------------------------------------------------------------------------------------")
-
+    print("------------------------------------------------"\
+            "------------------------------------------------------------------------")
+    cowsay.dragon(f"You completed the quiz!! You got {num_correct} out" \
+         "of {num}\n questions. A " +str(total_score_perc) + " %!!"\
+         "You can only sit on the\n throne if you got all the question."\
+         "So if\n you did, Congrats!!! The Throne is yours!!!\n If not try againg!!")
+    print("------------------------------------------------"\
+            "------------------------------------------------------------------------")
 #Randomise questions order to add difficulty for player who is playing multiple time
 #Allow user to select however many questions they want; IMPORTANT: max 15 questions
 def prep_char_questions(questions, number_questions):
+    '''randomize question order and retrieve number of question input bt user'''
     number_questions = min(number_questions, len(questions))
     return random.sample(list(questions.items()), k=number_questions)
 
@@ -52,6 +63,7 @@ def prep_char_questions(questions, number_questions):
 #add num_correct count
 #seperate into two functions for readability
 def ask_char_questions(question, alternatives):
+    '''randomize choice order and check answer'''
     correct_ans = alternatives[0]
     ordered_alternatives = random.sample(alternatives, k=len(alternatives))
 
@@ -66,16 +78,19 @@ def ask_char_questions(question, alternatives):
 #Label alternative choice
 #if user enter choice that is not available allow them to answer again
 def get_char_answer(question, alternatives):
+    '''retrieve answer and choices'''
     print(f"{question}")
-    print("------------------------------------------------------------------------------------------------------------------------")
+    print("------------------------------------------------"\
+            "------------------------------------------------------------------------")
     labeled_choice = dict(zip(ascii_uppercase, alternatives))
     for label, alternative in labeled_choice.items():
         print(f" {label}. {alternative}")
- 
     while (ans_label := input("Enter your answer (A, B, C, or D): ")) not in labeled_choice:
-        print(f"Not an option. Be sure to capitalise your answer. Please choose between A, B, C, or D")
-    return labeled_choice[ans_label]  
+        print("Not an option. Be sure to capitalise your answer."\
+             "Please choose between A, B, C, or D")
+    return labeled_choice[ans_label]
 def play_again():
+    '''let user play again'''
     user_command = input("Do you want to try and fight for the Throne again? (yes or no): ")
     user_command = user_command.upper()
 
@@ -88,6 +103,5 @@ if __name__ == "__main__":
 
     while play_again():
         new_game()
-        
-    
+
     cowsay.dragon("Thanks for playing!!")
