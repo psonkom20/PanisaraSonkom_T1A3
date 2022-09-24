115bd680a24ad6ead9c5951d214df644c18cf20b
Panisara Sonkom <[psonkom342@gmail.com]>
2022-09-25 02:18:16 +1000

    Updated bash script

--- python/PanisaraSonkom_T1A3/source_code/run_script.sh
+++ python/PanisaraSonkom_T1A3/source_code/run_script.sh
@@ -2,9 +2,19 @@
 python3 main.py $1
 
 
+python3 -m pip install cowsay
+
 if ! [[ -x "$ (command -v python)"]]
 then
-
+    python_version = "$(python -v 2>&1)"
+    if [[python_version == "Python 3"* ]]
+    then
+        python3 main.py
+    else
+    echo "Error: Your running an older version of python.
+     Please update to a newer version; https://www.python.org/downloads/ " >&2``
+    fi
+else
     echo 'Error:
     This program runs on Python, but its look like Python is not currently installed.
     to install Python, please refer to https//installpython3.com' >&2
