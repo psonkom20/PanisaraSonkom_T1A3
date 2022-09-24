126e4673b89bd011c8023a6938ec037607269309
Panisara Sonkom <[psonkom342@gmail.com]>
2022-09-25 01:09:18 +1000

    update bash

--- python/PanisaraSonkom_T1A3/source_code/run_script.sh
+++ python/PanisaraSonkom_T1A3/source_code/run_script.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 python3 main.py $1
 
+
 if ! [[ -x "$ (command -v python)"]]
 then
-    if command -v cowsay >/dev/null
-        then
-            echo "Cowsay is installed"
-        else
-            echo "Cowsay is not installed"
+
     echo 'Error:
     This program runs on Python, but its look like Python is not currently installed.
     to install Python, please refer to https//installpython3.com' >&2
